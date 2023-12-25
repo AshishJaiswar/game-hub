a8719b207ba3732909b3ba4542c952464a43900f
AshishJaiswar <Ajaiswar123456789@gmail.com>
2023-12-25 22:03:27 +0530

    Fetch the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
 import Layout from "./Layout.tsx";
+import GameGrid from "./components/GameGrid.tsx";
 
 function App() {
   return (
     <>
       <Layout>
         <RouterProvider router={router} />
+        <GameGrid />
       </Layout>
     </>
   );

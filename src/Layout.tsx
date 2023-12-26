27757c43212c7498c2ef90b8372bf4189deb54c7
AshishJaiswar <Ajaiswar123456789@gmail.com>
2023-12-26 15:47:05 +0530

    Fetch the genres

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import Navbar from "./components/Navbar";
+import GenreList from "./components/GenreList";
 
 interface Props {
   children: ReactNode;
@@ -18,7 +19,9 @@ function Layout({ children }: Props) {
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">Aside</GridItem>
+        <GridItem area="aside">
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem area="main">{children}</GridItem>
     </Grid>

46a6fbb41b391ffc346cd9e3ac93cff6caadd5c7
AshishJaiswar <Ajaiswar123456789@gmail.com>
2023-12-30 21:32:52 +0530

    Sort feature implemented

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,6 +11,7 @@ import SortSelector from "../components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 function Home() {
   const [gameQuery, setGameQuery] = useState({} as GameQuery);
@@ -45,7 +46,12 @@ function Home() {
               setGameQuery({ ...gameQuery, platform })
             }
           />
-          <SortSelector />
+          <SortSelector
+            sortOrder={gameQuery.sortOrder}
+            onSelectSortOrder={(sortOrder) =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
+          />
         </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
